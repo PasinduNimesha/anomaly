c6ce7c314d73880b9b6c1002f1046429e5e58edd
Pasindu Rathnayaka <86573066+PasinduNimesha@users.noreply.github.com>
2026-01-16 23:30:49 +0530

    Limit configuration to Sixray dataset only

--- config.py
+++ config.py
@@ -16,37 +16,37 @@ from tensorflow.keras import models
 import tensorflow as tf
 from model import ProposedModel, getAssembledModel
 
-numDatasets = 5
+numDatasets = 1
 
 tr_folder = "datasets/sixray/normal/" # path to training dataset
 
 te_folders = [] # path to test datasets
 te_folders.append("datasets/sixray/abnormal/") 
-te_folders.append("datasets/gdxray/abnormal/") 
-te_folders.append("datasets/opixray/abnormal/") 
-te_folders.append("datasets/compass/abnormal/") 
-te_folders.append("datasets/mvtec/abnormal/") 
+# te_folders.append("datasets/gdxray/abnormal/") 
+# te_folders.append("datasets/opixray/abnormal/") 
+# te_folders.append("datasets/compass/abnormal/") 
+# te_folders.append("datasets/mvtec/abnormal/") 
 
 res_fake = [] # path to store reconstructed images for each dataset
 res_fake.append("datasets/sixray/results/fake/")
-res_fake.append("datasets/gdxray/results/fake/")
-res_fake.append("datasets/opixray/results/fake/")
-res_fake.append("datasets/compass/results/fake/")
-res_fake.append("datasets/mvtec/results/fake/")
+# res_fake.append("datasets/gdxray/results/fake/")
+# res_fake.append("datasets/opixray/results/fake/")
+# res_fake.append("datasets/compass/results/fake/")
+# res_fake.append("datasets/mvtec/results/fake/")
 
 res_real = [] # path to store real images for each dataset
 res_real.append("datasets/sixray/results/real/")
-res_real.append("datasets/gdxray/results/real/")
-res_real.append("datasets/opixray/results/real/")
-res_real.append("datasets/compass/results/real/")
-res_real.append("datasets/mvtec/results/real/")
+# res_real.append("datasets/gdxray/results/real/")
+# res_real.append("datasets/opixray/results/real/")
+# res_real.append("datasets/compass/results/real/")
+# res_real.append("datasets/mvtec/results/real/")
 
 res_disp = [] # path to store disparity maps for each dataset
 res_disp.append("datasets/sixray/results/disp/")
-res_disp.append("datasets/gdxray/results/disp/")
-res_disp.append("datasets/opixray/results/disp/")
-res_disp.append("datasets/compass/results/disp/")
-res_disp.append("datasets/mvtec/results/disp/")
+# res_disp.append("datasets/gdxray/results/disp/")
+# res_disp.append("datasets/opixray/results/disp/")
+# res_disp.append("datasets/compass/results/disp/")
+# res_disp.append("datasets/mvtec/results/disp/")
 
 x_train = [] # contains training patches
 x_test = [] # contains test patches
